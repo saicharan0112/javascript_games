8e06415054b01424f6228a3fc34be52b46fa27fd
Sai Charan <saicharan00112@gmail.com>
2024-12-26 03:44:06 +0530

    setup the html id for the game board properly for easy access to small 9x9 grids

--- sudoko/game.js
+++ sudoko/game.js
@@ -1,20 +1,49 @@
-mainTxt = document.getElementById("board").innerHTML;
-j=0
-for (let k = 0; k < 9; k++) {
-    i = k%3;
-    divTxt = "<div id=\"user_btns\">\
-            <button  disabled class=\"tile"+i+"\" id=\""+k+"0\"></button>\
-            <button  disabled class=\"tile"+i+"\" id=\""+k+"1\"></button>\
-            <button  disabled class=\"tile"+i+"\" id=\""+k+"2\"></button>\
-            <button  disabled class=\"tile"+(i+3)+"\" id=\""+k+"3\"></button>\
-            <button  disabled class=\"tile"+(i+3)+"\" id=\""+k+"4\"></button>\
-            <button  disabled class=\"tile"+(i+3)+"\" id=\""+k+"5\"></button>\
-            <button  disabled class=\"tile"+(i+6)+"\" id=\""+k+"6\"></button>\
-            <button  disabled class=\"tile"+(i+6)+"\" id=\""+k+"7\"></button>\
-            <button  disabled class=\"tile"+(i+6)+"\" id=\""+k+"8\"></button>\
-        </div>"
+var gameboard = [
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""],
+    ["","","","","","","","",""]
+]
 
+
+mainTxt = document.getElementById("board").innerHTML;
+var f=0
+for (let k = 0; k < 9; k++) {
+    
+    i = Math.floor(k/3)
+
+    if(k==3||k==6||f){i*=3;f=1;}
+
+    divTxt = "<div id=\"user_btns\">\
+            <button  disabled class=\"tile"+i+"\" id=\""+k+"0\">"+k+"</button>\
+            <button  disabled class=\"tile"+i+"\" id=\""+k+"1\">"+k+"</button>\
+            <button  disabled class=\"tile"+i+"\" id=\""+k+"2\">"+k+"</button>"
+
+    divTxt += "<button  disabled class=\"tile"+(i+1)+"\" id=\""+k+"3\">"+k+"</button>\
+            <button  disabled class=\"tile"+(i+1)+"\" id=\""+k+"4\">"+k+"</button>\
+            <button  disabled class=\"tile"+(i+1)+"\" id=\""+k+"5\">"+k+"</button>"
+
+    divTxt += "<button  disabled class=\"tile"+(i+2)+"\" id=\""+k+"6\">"+k+"</button>\
+            <button  disabled class=\"tile"+(i+2)+"\" id=\""+k+"7\">"+k+"</button>\
+            <button  disabled class=\"tile"+(i+2)+"\" id=\""+k+"8\">"+k+"</button>\
+        </div>"
+    
     mainTxt += divTxt;
 }
 
 document.getElementById("board").innerHTML = mainTxt;
+
+k=0;
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        gameboard[i][j] = k++;
+        document.getElementById(i+""+j).innerText = gameboard[i][j];
+        // gameboard[i][j] = val;
+    }
+    
+}
