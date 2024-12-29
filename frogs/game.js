4d36b9cca9f1ef824ac3f45ddd6115ded7ddecc7
Sai Charan <saicharan00112@gmail.com>
2024-12-29 23:48:39 +0530

    frogs game final version

--- frogs/game.js
+++ frogs/game.js
@@ -1,24 +1,49 @@
-var game = "";
-var board = ["0", "0", "", "0", "", "1", "1", "1", "1"];
-const frogButtons = document.getElementsByClassName("frogs");
+function generateBoard() {
+    const boardLength = 9;
+    const halfLength = Math.floor(boardLength / 2);
+
+    let zeros = Array(halfLength).fill("0");
+    let ones = Array(halfLength).fill("1");
+
+    const makeSomeEmpty = (arr) => {
+        const emptyCount = Math.floor(Math.random() * (arr.length / 2)) + 1; // Ensure at least 1 empty
+        for (let i = 0; i < emptyCount; i++) {
+            const randomIndex = Math.floor(Math.random() * arr.length);
+            arr[randomIndex] = ""; // Replace with empty
+        }
+    };
+
+    makeSomeEmpty(zeros);
+    makeSomeEmpty(ones);
+
+    zeros = zeros.sort(() => Math.random() - 0.5);
+    ones = ones.sort(() => Math.random() - 0.5);
+
+    return [...zeros, "", ...ones];
+}
 
 
+var board = generateBoard();
 
 function init() {
     game = "";
     for (let index = 0; index < board.length; index++) {
         if(board[index] == ""){
             game += `<button class="frogs" id="${index}">&nbsp&nbsp</button>`;
-        } else {
-            game += `<button class="frogs" id="${index}">${board[index]}</button>`;
+        } else if (board[index] == "0") {
+            game += `<button class="frogs" id="${index}">${board[index]}<img src="./assets/frog1.png" alt="Frog 0" class="frog-image" width=50px height=50px></button>`;
+        } else if (board[index] == "1") {
+            game += `<button class="frogs" id="${index}">${board[index]}<img src="./assets/frog0.png" alt="Frog 0" class="frog-image" width=50px height=50px></button>`;
         }
     }
     document.getElementById("board").innerHTML = game;
     eventListener();
 }
 
+init();
 
 function eventListener(){
+    const frogButtons = document.getElementsByClassName("frogs");
     for (let button of frogButtons) {
         button.addEventListener("click", (event) => {
             console.log("Clicked ID:", event.target.id);
@@ -27,12 +52,12 @@ function eventListener(){
             console.log(board);
             if (["0"].includes(board[id])){
                 if(board[id+1] == ""){
-                    console.log("shifting...")
+                    console.log("shifting..."+board[id])
                     tmp = board[id+1];
                     board[id+1] = board[id];
                     board[id] = tmp;
                 }else if(board[id+1] != board[id] && id < 7 && board[id+2] == ""){
-                    console.log("shifting...")
+                    console.log("shifting..."+board[id])
                     tmp = board[id+2];
                     board[id+2] = board[id];
                     board[id] = tmp;
@@ -42,12 +67,12 @@ function eventListener(){
 
             if (["1"].includes(board[id])){
                 if(board[id-1] == ""){
-                    console.log("shifting...")
+                    console.log("shifting... "+board[id]);
                     tmp = board[id-1];
                     board[id-1] = board[id];
                     board[id] = tmp;
                 } else if(board[id-1] != board[id] && id > 1 && board[id-2] == ""){
-                    console.log("shifting...")
+                    console.log("shifting... "+board[id]);
                     tmp = board[id-2];
                     board[id-2] = board[id];
                     board[id] = tmp;
@@ -55,11 +80,11 @@ function eventListener(){
             } 
  
             console.log(board);
+            if(checkWinCase(board)){return;}
             init();
             });
     }
 }
-
 
 function getButtonValues() {
     const buttons = document.querySelectorAll("#board button"); // Correctly select buttons within the board div
@@ -72,5 +97,16 @@ function getButtonValues() {
     return values; // Return the list of values
 }
 
-init();
+function checkWinCase(board) {
+    ones = board.slice(0,4);
+    zeros = board.slice(5,board.length);
+    if((ones.includes("0")) || (zeros.includes("1"))) {
+        return false;
+    } else {
+        document.getElementById("response").innerHTML="You won!";
+        return true;
+    } 
+};
+
+
 document.getElementById("reset").addEventListener("click", () => location.reload())
